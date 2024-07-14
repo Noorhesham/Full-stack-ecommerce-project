0f013d60af580473dc8307a53b9c2e4c945c3bce
Noorhesham <noordragon2004@gmail.com>
2024-07-14 17:40:37 +0200

    remove animation

---
+++ app/(home)/loading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./naruto.css";
+const loading = () => {
+  return (
+    <div id="naruto" className=" pt-20">
+      <section className="head"></section>
+      <section className="torso"></section>
+      <section className="legs"></section>
+      <section className="shadow"></section>
+    </div>
+  );
+};
+
+export default loading;
