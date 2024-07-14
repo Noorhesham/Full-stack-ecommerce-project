4dcaa1d1024baa8c26aaa0c06a9cd9fcab4a54fa
Noorhesham <noordragon2004@gmail.com>
2024-07-14 19:53:23 +0200

    remove animation

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,18 +1,10 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
-import { getServerSession } from "next-auth";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import NavBar from "../components/NavBar";
-import User from "@/lib/database/models/UserModel";
-import { redirect } from "next/navigation";
 import connect from "@/lib/database/connect";
 import React from "react";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 import Footer from "../components/Footer";
-import { constructMetadata } from "@/lib/utils";
-const inter = Inter({ subsets: ["latin"] });
 
 //todos
 /* 
