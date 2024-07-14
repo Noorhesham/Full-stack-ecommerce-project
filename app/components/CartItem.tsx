1cd650a70bba3116c29d0dd20cd70df011b112e8
Noorhesham <noordragon2004@gmail.com>
2024-07-14 18:50:28 +0200

    3d model

--- app/components/CartItem.tsx
+++ app/components/CartItem.tsx
@@ -43,23 +43,23 @@ const CartItem = ({
   };
   const variantsItems = product.variants || variants;
   const basePrice = calculateFinalPrice(product.price, variantsItems, product.variations);
-
+  console.log(product)
   return (
     <div className=" space-y-3 py-2">
       <div className="flex items-start justify-between gap-4">
         <div className=" flex items-center space-x-4">
           <div className=" relative aspect-square w-16 h-16 min-w-fit overflow-hidden rounded">
-            <Image src={product.images[0]?.imgUrl} alt={product.name} fill className=" object-cover" />
+            <Image src={product.images?.[0]?.imgUrl} alt={product.name} fill className=" object-cover" />
           </div>
           <div className="flex flex-col self-start">
             <Link href={`/product/${product._id}`} className=" hover:text-amber-400 hover:underline duration-150 line-clamp-1 text-sm font-medium mb-1">
               {product.name}
             </Link>{" "}
             <Link
-              href={`/products?category=${product.category.name}`}
+              href={`/products?category=${product.category?.name}`}
               className=" hover:text-amber-400 hover:underline duration-150 line-clamp-1 text-sm capitalize text-muted-foreground "
             >
-              {product.category.name}
+              {product.category?.name}
             </Link>
             <Link
               href={`/profile/${product.creator?._id}`}
